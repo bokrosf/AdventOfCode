c48d0712a1116d4c8b51a64087bec47b5a4079e6
Bokros Ferenc <bokros.feri@hotmail.com>
2020-07-06 22:52:45 +0200

    Add project files.

---
+++ AdventOfCode/DayBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode
+{
+    public abstract class DayBase
+    {
+        public abstract string GetInputFileName();
+        public abstract void SolveFirstPart();
+        public abstract void SolveSecondPart();
+    }
+}
---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdventOfCode
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("AdventOfCode");
+        }
+    }
+}
