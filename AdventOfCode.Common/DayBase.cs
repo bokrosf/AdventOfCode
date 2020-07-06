0787e66458370785c21c6466d07826d3d60dc9f1
Bokros Ferenc <bokros.feri@hotmail.com>
2020-07-06 23:31:12 +0200

    Common project has been added to the solution and DayBase class has been moved to the Common project from AdventOfCode project.
    Day01 class has been added to Year2018 project and Path class has been renamed to PathResolver.

--- AdventOfCode.Common/DayBase.cs
+++ AdventOfCode.Common/DayBase.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace AdventOfCode
+namespace AdventOfCode.Common
 {
     public abstract class DayBase
     {
---
+++ AdventOfCode.Year2018/Days/Day01.cs
@@ -0,0 +1,24 @@
+﻿using AdventOfCode.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode.Year2018.Days
+{
+    public class Day01 : DayBase
+    {
+        private const string InputFileName = "input_01.txt";
+
+        public override string GetInputFileName() => PathResolver.ResolveInputFileNamePath(InputFileName);
+
+        public override void SolveFirstPart()
+        {
+            Console.WriteLine("First part's solution has not been completed.");
+        }
+
+        public override void SolveSecondPart()
+        {
+            Console.WriteLine("Second part's solution has not been completed.");
+        }
+    }
+}
